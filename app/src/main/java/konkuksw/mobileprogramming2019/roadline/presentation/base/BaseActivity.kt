33250fe312c4199f02f046500ab7ca2a6eda53bb
enan <hant2258@gmail.com>
2021-04-06 15:42:19 +0900

    ADD BaseActivity, Fragment, ViewModel

---
+++ app/src/main/java/konkuksw/mobileprogramming2019/roadline/presentation/base/BaseActivity.kt
@@ -0,0 +1,18 @@
+package konkuksw.mobileprogramming2019.roadline.presentation.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+
+abstract class BaseActivity<T : ViewDataBinding>(
+    @LayoutRes private val layoutResourceId: Int) : AppCompatActivity() {
+    lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResourceId)
+    }
+}
---
+++ app/src/main/java/konkuksw/mobileprogramming2019/roadline/presentation/base/BaseViewModel.kt
@@ -0,0 +1,7 @@
+package konkuksw.mobileprogramming2019.roadline.presentation.base
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel() : ViewModel() {
+
+}
