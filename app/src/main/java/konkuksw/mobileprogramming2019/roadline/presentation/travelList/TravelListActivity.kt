60ab72ca11fcc93db05ba8f933a3f41900f1e4e8
enan <hant2258@gmail.com>
2021-04-06 19:59:40 +0900

    ADD TravelListAdapter

--- app/src/main/java/konkuksw/mobileprogramming2019/roadline/presentation/travelList/TravelListActivity.kt
+++ app/src/main/java/konkuksw/mobileprogramming2019/roadline/presentation/travelList/TravelListActivity.kt
@@ -1,5 +1,6 @@
 package konkuksw.mobileprogramming2019.roadline.presentation.travelList
 
+import android.os.Bundle
 import androidx.activity.viewModels
 import konkuksw.mobileprogramming2019.roadline.R
 import konkuksw.mobileprogramming2019.roadline.databinding.ActivityTravelListBinding
@@ -9,4 +10,10 @@ class TravelListActivity : BaseActivity<ActivityTravelListBinding>(
     R.layout.activity_travel_list
 ) {
     private val viewModel: TravelListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.rvTravel.adapter = TravelListAdapter()
+    }
 }
